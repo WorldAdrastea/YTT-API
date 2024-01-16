ac66aa967785e12f65fde07077820524badae408
William Yau <122402171+WorldAdrastea@users.noreply.github.com>
2024-01-16 22:58:11 +0000

    Update middlewares.ts
    console logging to compare wtih local version

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -11,12 +11,14 @@ export const isOwner = async (req: express.Request, res: express.Response, next:
         const currentUserId = get(req, 'identity._id') as string;
 
         if (!currentUserId || currentUserId.toString() !== id) {
+            console.log('isOwner: Unauthorized - Current User ID:', currentUserId);
             return res.sendStatus(403);
         }
 
+        console.log('isOwner: Authorized - Current User ID:', currentUserId);
         next();
     } catch (error) {
-        console.log(error);
+        console.log('isOwner: Error -', error);
         return res.sendStatus(500);
     }
 }
@@ -27,20 +29,23 @@ export const isAuthenticated = async (req: express.Request, res: express.Respons
         const sessionToken = req.cookies['YTT_AUTH'];
 
         if (!sessionToken) {
+            console.log('isAuthenticated: No Session Token');
             return res.sendStatus(403);
         }
 
         const existingUser = await getUserBySessionToken(sessionToken);
 
         if (!existingUser) {
+            console.log('isAuthenticated: Invalid Session Token');
             return res.sendStatus(403);
         }
 
         merge(req, { identity: existingUser});
 
+        console.log('isAuthenticated: User Authenticated - User ID:', existingUser._id);
         return next();
     } catch (error) {
-        console.log(error);
+        console.log('isAuthenticated: Error -', error);
         return res.sendStatus(500);
     }
 }
